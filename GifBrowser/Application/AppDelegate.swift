4b63d468767e5bde77f60275de84cbe44240cdd7
Guilherme <guilherme.coelho@bancointer.com.br>
2020-03-08 09:04:06 -0300

    Project init - Add gitignore

---
+++ GifBrowser/Application/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  GifBrowser
+//
+//  Created by Guilherme Maciel de Aguiar Nunes Coelho on 08/03/20.
+//  Copyright © 2020 gmanc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
