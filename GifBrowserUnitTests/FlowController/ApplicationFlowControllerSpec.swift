ba0d399c2f6d00d22cfa64c4a3b093b4fcdd1fe3
Guilherme <guilherme.coelho@bancointer.com.br>
2020-03-08 17:59:55 -0300

    Create test target

---
+++ GifBrowserUnitTests/FlowController/ApplicationFlowControllerSpec.swift
@@ -0,0 +1,9 @@
+//
+//  ApplicationFlowControllerSpec.swift
+//  GifBrowserUnitTests
+//
+//  Created by Guilherme Maciel de Aguiar Nunes Coelho on 08/03/20.
+//  Copyright © 2020 gmanc. All rights reserved.
+//
+
+import Foundation
